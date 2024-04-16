77e802286f16becf28957b407401cbbfdabc1708
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-15 23:58:20 -0300

    finishing dynamoDTO💎

--- src/shared/infra/dto/user_dynamo_dto.ts
+++ src/shared/infra/dto/user_dynamo_dto.ts
@@ -1,65 +1,63 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { User } from '../../domain/entities/user'
-import { STATE } from '../../domain/enums/state_enum'
 
 type UserDynamoDTOProps = {
-  id: string
   name: string
   email: string
-  state: STATE
+  password: string
 }
 
 export class UserDynamoDTO {
-  private id: string
   private name: string
   private email: string
-  private state: STATE
+  private password: string
 
-  constructor (props: UserDynamoDTOProps) {
-    this.id = props.id
+  constructor(props: UserDynamoDTOProps) {
     this.name = props.name
     this.email = props.email
-    this.state = props.state
+    this.password = props.password
   }
 
   static fromEntity(user: User): UserDynamoDTO {
     return new UserDynamoDTO({
-      id: user.id.toString(),
       name: user.name,
       email: user.email,
-      state: user.state as STATE
+      password: user.password,
     })
   }
 
   toDynamo() {
     return {
-      'entity': 'user',
-      'id': this.id,
-      'name': this.name,
-      'email': this.email,
-      'state': this.state,
+      entity: 'user',
+      name: this.name,
+      email: this.email,
+      password: this.password,
     }
   }
 
   static fromDynamo(userData: any) {
-    const id = userData['id'] && userData['id']['S'] ? userData['id']['S'] : null
-    const name = userData['name'] && userData['name']['S'] ? userData['name']['S'] : null
-    const email = userData['email'] && userData['email']['S'] ? userData['email']['S'] : null
-    const state = userData['state'] && userData['state']['S'] ? userData['state']['S'] : null
+    const name =
+      userData['name'] && userData['name']['S'] ? userData['name']['S'] : null
+    const email =
+      userData['email'] && userData['email']['S']
+        ? userData['email']['S']
+        : null
+    const password =
+      userData['password'] && userData['password']['S']
+        ? userData['password']['S']
+        : null
     return new UserDynamoDTO({
-      id,
       name,
       email,
-      state
+      password,
     })
   }
 
   toEntity() {
     return new User({
-      id: Number(this.id),
       name: this.name,
       email: this.email,
-      state: this.state
+      password: this.password,
     })
   }
 }
