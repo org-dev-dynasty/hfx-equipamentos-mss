ed32a95a415464d334281a4292e144f952c29d5a
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-18 01:44:24 -0300

    dynamo product table✅

--- iac/iac/template_dynamo_table.ts
+++ iac/iac/template_dynamo_table.ts
@@ -1,25 +1,22 @@
 import { Construct } from 'constructs'
 import { Table, AttributeType, BillingMode } from 'aws-cdk-lib/aws-dynamodb'
 import { RemovalPolicy } from 'aws-cdk-lib'
-import env from '../../index'
 
 export class TemplateDynamoTable extends Construct {
   public table: Table
 
-  constructor(scope: Construct, constructId: string) {
+  constructor(scope: Construct, constructId: string, dynamoTableName: string) {
     super(scope, constructId)
 
-    if (env.DYNAMO_TABLE_NAME === undefined) throw new Error('DYNAMO_TABLE_NAME is undefined')
-
     this.table = new Table(this, 'HfxMssTable', {
-      tableName: env.DYNAMO_TABLE_NAME,
+      tableName: dynamoTableName,
       partitionKey: {
         name: 'PK',
-        type: AttributeType.STRING
+        type: AttributeType.STRING,
       },
       sortKey: {
         name: 'SK',
-        type: AttributeType.STRING
+        type: AttributeType.STRING,
       },
       billingMode: BillingMode.PAY_PER_REQUEST,
       removalPolicy: RemovalPolicy.DESTROY,
