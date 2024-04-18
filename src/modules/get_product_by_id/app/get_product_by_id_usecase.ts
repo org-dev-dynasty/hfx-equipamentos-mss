55daf2e400ed2e1403e1e5f6cdb6d2b135ceca81
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-18 17:00:36 -0300

    fixing usecase only to pass a id✅

--- src/modules/get_product_by_id/app/get_product_by_id_usecase.ts
+++ src/modules/get_product_by_id/app/get_product_by_id_usecase.ts
@@ -2,21 +2,15 @@
 import { Product } from '../../../shared/domain/entities/product'
 import { IProductRepository } from '../../../shared/domain/repositories/product_repository_interface'
 import { InvalidRequest } from '../../../shared/helpers/errors/ModuleError'
-import { MissingParameters } from '../../../shared/helpers/errors/controller_errors'
 
 export class GetProductByIdUsecase {
   constructor(private productRepository: IProductRepository) {}
 
-  async execute(body: { [key: string]: any }): Promise<Product> {
-    if (!body) {
-      throw new InvalidRequest('Body')
+  async execute(id: string): Promise<Product> {
+    if (!id) {
+      throw new InvalidRequest('id')
     }
-    if (!body.products.id) {
-      throw new MissingParameters('id')
-    }
-    const product = await this.productRepository.getProductById(
-      body.products.id,
-    )
+    const product = await this.productRepository.getProductById(id)
     return product
   }
 }
