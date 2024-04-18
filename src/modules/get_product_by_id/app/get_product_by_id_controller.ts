3c9c753840d6e846d081ffd45de3f7723bca2768
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-18 16:17:54 -0300

    finishing get product by id controller (needs to verify query string params)✅

---
+++ src/modules/get_product_by_id/app/get_product_by_id_controller.ts
@@ -0,0 +1,56 @@
+import { InvalidRequest } from '../../../shared/helpers/errors/ModuleError'
+import {
+  MissingParameters,
+  WrongTypeParameters,
+} from '../../../shared/helpers/errors/controller_errors'
+import { EntityError } from '../../../shared/helpers/errors/domain_errors'
+import { NoItemsFound } from '../../../shared/helpers/errors/usecase_errors'
+import {
+  BadRequest,
+  InternalServerError,
+  NotFound,
+  OK,
+} from '../../../shared/helpers/external_interfaces/http_codes'
+import { HttpRequest } from '../../../shared/helpers/external_interfaces/http_models'
+import { GetProductByIdUsecase } from './get_product_by_id_usecase'
+import { GetProductByIdViewModel } from './get_product_by_id_viewmodel'
+
+export class GetProductByIdController {
+  public usecase: GetProductByIdUsecase
+
+  constructor(usecase: GetProductByIdUsecase) {
+    this.usecase = usecase
+  }
+
+  public async handle(request: HttpRequest) {
+    try {
+      if (!request) {
+        throw new InvalidRequest()
+      }
+      if (!request.body.queryStringParameters) {
+        throw new MissingParameters('queryStringParameters')
+      }
+
+      const queryParams = request.body.queryStringParameters
+
+      const response = await this.usecase.execute(queryParams)
+      const viewmodel = new GetProductByIdViewModel(response)
+      return new OK(viewmodel.toJSON())
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      if (error instanceof NoItemsFound) {
+        return new NotFound(error.message)
+      }
+      if (
+        error instanceof MissingParameters ||
+        error instanceof WrongTypeParameters ||
+        error instanceof EntityError
+      ) {
+        return new BadRequest(error.message)
+      }
+      if (error instanceof Error) {
+        return new InternalServerError(error.message)
+      }
+    }
+  }
+}
