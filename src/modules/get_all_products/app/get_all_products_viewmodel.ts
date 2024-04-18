3876109b0184ac39aeb69b60c1ae87dd56688346
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-18 01:45:33 -0300

    finishing viewmodel✅

---
+++ src/modules/get_all_products/app/get_all_products_viewmodel.ts
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ProductProps } from '../../../shared/domain/entities/product'
+
+export class ProductViewModel {
+  private id: string
+  private name: string
+  private description: string
+  private models?: string[]
+  private categories?: string[]
+  private attributes?: Record<string, any>[]
+  private videos?: string[]
+
+  constructor(product: ProductProps) {
+    this.id = product.id
+    this.name = product.name
+    this.description = product.description
+    this.models = product.models
+    this.categories = product.categories
+    this.attributes = product.attributes
+    this.videos = product.videos
+  }
+
+  toJSON() {
+    return {
+      id: this.id,
+      name: this.name,
+      description: this.description,
+      models: this.models,
+      categories: this.categories,
+      attributes: this.attributes,
+      videos: this.videos,
+    }
+  }
+}
+
+export class GetAllProductViewModel {
+  private products: ProductViewModel[]
+
+  constructor(products: ProductProps[]) {
+    this.products = products.map((product) => new ProductViewModel(product))
+  }
+
+  toJSON() {
+    return {
+      products: this.products.map((product) => product.toJSON()),
+      message: 'Products have been retrieved successfully.',
+    }
+  }
+}
