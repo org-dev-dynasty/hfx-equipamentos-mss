517befc11671eac70e290448f6a86165a35d3e55
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-20 17:26:22 -0300

    creating presenter of delete model of product✅

---
+++ src/modules/delete_model_of_product/app/delete_model_of_product_presenter.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { Environments } from '../../../shared/environments'
+import {
+  LambdaHttpRequest,
+  LambdaHttpResponse,
+} from '../../../shared/helpers/external_interfaces/http_lambda_requests'
+import { DeleteModelOfProductController } from './delete_model_of_product_controller'
+import { DeleteModelOfProductUsecase } from './delete_model_of_product_usecase'
+
+const repo = Environments.getProductRepo()
+const usecase = new DeleteModelOfProductUsecase(repo)
+const controller = new DeleteModelOfProductController(usecase)
+
+export async function updateProductPresenter(event: Record<string, any>) {
+  const httpRequest = new LambdaHttpRequest(event)
+  const response = await controller.handle(httpRequest)
+  const httpResponse = new LambdaHttpResponse(
+    response?.body,
+    response?.statusCode,
+    response?.body.queryStringParameters,
+  )
+
+  return httpResponse.toJSON()
+}
+
+export async function handler(event: any, context: any) {
+  const response = await updateProductPresenter(event)
+  return response
+}
