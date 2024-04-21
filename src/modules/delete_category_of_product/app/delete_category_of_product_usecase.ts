06601a7410ef74d374e588d7e4006a5f9010ba69
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-21 13:23:29 -0300

    finishing delete category usecase✅

---
+++ src/modules/delete_category_of_product/app/delete_category_of_product_usecase.ts
@@ -0,0 +1,38 @@
+import { IProductRepository } from '../../../shared/domain/repositories/product_repository_interface'
+import { EntityError } from '../../../shared/helpers/errors/domain_errors'
+
+export class DeleteCategoryOfProductUsecase {
+  constructor(private repo: IProductRepository) {}
+
+  async execute(productId: string, categoryId: string) {
+    const product = await this.repo.getProductById(productId)
+
+    if (!product) {
+      throw new EntityError('Product not found')
+    }
+
+    const updatedCategories =
+      product.categories && product.categories.length > 0
+        ? product.categories.filter((category) => category.split('#')[0] !== categoryId)
+        : []
+
+    const updatedAttributes = product.attributes
+      ? product.attributes.filter((attribute) => {
+        const attributeCategoryId = attribute.CategoryId.split('#')[0]
+        return attributeCategoryId !== categoryId
+      })
+      : []
+
+    const updatedProduct = await this.repo.updateProduct(
+      productId,
+      undefined,
+      undefined,
+      updatedCategories,
+      undefined,
+      updatedAttributes,
+      undefined,
+    )
+
+    return updatedProduct
+  }
+}
