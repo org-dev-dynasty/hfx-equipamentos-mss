1d03f7cf1ebe8f1736a7eaad0a6d16a4baf25312
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-22 19:24:25 -0300

    adding littleDescription to return in data object✅

--- src/modules/update_product/app/update_product_viewmodel.ts
+++ src/modules/update_product/app/update_product_viewmodel.ts
@@ -5,6 +5,7 @@ export class UpdateProductViewModel {
   private id: string
   private name: string
   private description: string
+  private littleDescription?: string[]
   private models?: string[] | null
   private categories?: string[] | null
   private attributes?: Record<string, any>[] | null
@@ -16,6 +17,7 @@ export class UpdateProductViewModel {
     this.id = product.id
     this.name = product.name
     this.description = product.description
+    this.littleDescription = product.littleDescription
     this.models = product.models
     this.categories = product.categories
     this.attributes = product.attributes
@@ -29,6 +31,7 @@ export class UpdateProductViewModel {
       id: this.id,
       name: this.name,
       description: this.description,
+      littleDescription: this.littleDescription,
       models: this.models,
       categories: this.categories,
       attributes: this.attributes,
